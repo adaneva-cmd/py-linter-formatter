11c9914e4cc0e9a47b287fee210e87c8a95cb264
Sue <sanchezsilvasue@gmail.com>
2026-05-15 19:58:57 +0200

    fixed indentation

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-     return {
+    return {
         "path": file_path,
         "status": "failed" if errors else "passed",
         "errors": [format_linter_error(error) for error in errors]
@@ -17,7 +17,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-     return [
+    return [
         format_single_linter_file(filename, errors)
         for filename, errors in linter_report.items()
     ]
